24b755d0cc3dd2575fe98224bd3a82ea5bde30e9
Ron-Chang <ron.hsien.chang@gmail.com>
2020-09-10 11:29:40 +0800

    Add bar and slicer and rename timer instead of snaptimer

--- ronchang/__init__.py
+++ ronchang/__init__.py
@@ -17,7 +17,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import time
+
 from .timer import Timer
+from .slicer import Slicer
+from .bar import Bar
 
 
 class Demo:
@@ -54,3 +58,48 @@ class Demo:
             foo -= 1
             Timer(interval=1, start=start, EXTRA='Extra information goes here')
 
+    # @classmethod
+    # def Slicer(cls, **kwargs):
+        # if 'input_data' in kwargs and isinstance(kwargs['input_data'], list):
+            # method = getattr(ContainerSlicer, 'split_list')
+            # src = kwargs['input_data']
+        # elif 'input_data' in kwargs and isinstance(kwargs['input_data'], dict):
+            # method = getattr(ContainerSlicer, 'split_dict')
+            # src = kwargs['input_data']
+        # elif 'number' in kwargs and type(kwargs['number']) in [str, int]:
+            # method = getattr(ContainerSlicer, 'split_number')
+            # src = kwargs['number']
+        # else:
+            # raise ValueError(f'Invalid type or error input! {kwargs}')
+        # results = method(**kwargs)
+        # print(f'{"":=^90}')
+        # print(f'Input:\n\t{src}')
+        # print(f'Results:\n\t{results}')
+        # print(f'Amount: {len(results)}')
+        # print(f'{"":-^90}')
+        # for index, result in enumerate(results):
+            # print(f'slice {index} | len == {len(result)} | {result}')
+        # print(f'{"":=^90}')
+
+    # ## ################## Demo a: split list  ################## ##
+    # demo(input_data=[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]], slice_length=4)
+    # ## ################## Demo a: split list  ################## ##
+
+    # ## ################## Demo b: split dict  ################## ##
+    # input_data = {
+        # '1': {'name': 'Ajax', 'age': 25},
+        # '2': {'name': 'Roni', 'age': 29},
+        # '3': {'name': 'Jeffrey', 'age': 34},
+        # '4': {'name': 'Josh', 'age': 33},
+        # '5': {'name': 'Kevin', 'age': 26},
+        # '6': {'name': 'Jeque', 'age': 23},
+        # '7': {'name': 'Amy', 'age': 32},
+        # '8': {'name': 'May', 'age': 49},
+        # '9': {'name': 'Ronnie', 'age': 22},
+    # }
+    # demo(input_data=input_data, slice_length=2)
+    # ## ################## Demo b: split dict  ################## ##
+
+    # ## ################## Demo c: split a number ################## ##
+    # demo(number=40, slice_length=7)
+    # ## ################## Demo c: split a number ################## ##
